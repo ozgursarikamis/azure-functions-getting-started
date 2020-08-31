d9a90bfc7471ac47667fd6ee7c1b732c21a3bac1
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-31 21:58:19 +0300

    Queue Output binding

--- OnPaymentReceive.cs
+++ OnPaymentReceive.cs
@@ -1,4 +1,3 @@
-using System;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -7,20 +6,34 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System;
 
 namespace GettingStarted
 {
+	public class Order
+	{
+		public string OrderId { get; internal set; }
+		public string ProductId { get; internal set; }
+		public string EmailId { get; internal set; }
+	}
 	public static class OnPaymentReceive
 	{
 		[FunctionName("OnPaymentReceive")]
 		public static async Task<IActionResult> Run(
 			[HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+			[Queue("orders")] IAsyncCollector<Order> orderQueue,
 			ILogger log)
 		{
 			log.LogInformation("C# HTTP trigger function processed a request.");
 
 			string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-			dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+			var order = JsonConvert.DeserializeObject<Order>(requestBody);
+			await orderQueue.AddAsync(order);
+
+			log.LogInformation($"Order ID: {order.OrderId}");
+			log.LogInformation($"Product ID: {order.ProductId}");
+			log.LogInformation($"Email ID: {order.EmailId}");
 
 			return new JsonResult(new { Message = "Thank you for the purchase" });
 		}
