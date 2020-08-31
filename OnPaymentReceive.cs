1cb489614641cfc3dbba28a0779a8df4f179d03d
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-31 20:45:55 +0300

    first function

--- OnPaymentReceive.cs
+++ OnPaymentReceive.cs
@@ -10,26 +10,19 @@ using Newtonsoft.Json;
 
 namespace FuncWithCli
 {
-    public static class OnPaymentReceive
-    {
-        [FunctionName("OnPaymentReceive")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+	public static class OnPaymentReceive
+	{
+		[FunctionName("OnPaymentReceive")]
+		public static async Task<IActionResult> Run(
+			[HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+			ILogger log)
+		{
+			log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+			string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+			dynamic data = JsonConvert.DeserializeObject(requestBody);
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
-        }
-    }
+			return new JsonResult(new { Message = "Thank you for the purchase" });
+		}
+	}
 }
