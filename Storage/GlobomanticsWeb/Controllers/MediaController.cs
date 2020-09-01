d565811590cd3fb39655b9b08f81ae4bb4fe5b39
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-09-02 00:41:06 +0300

    upload image to Azure Storage

--- Storage/GlobomanticsWeb/Controllers/MediaController.cs
+++ Storage/GlobomanticsWeb/Controllers/MediaController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using GlobomanticsWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 
 namespace GlobomanticsWeb.Controllers
@@ -10,6 +15,31 @@ namespace GlobomanticsWeb.Controllers
         public MediaController(IConfiguration appConfig)
         {
             config = appConfig;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //[Authorize]
+        public async Task<IActionResult> Index(ImageUploadModel model)
+        {
+            //upload image after authorizing user
+            var containerClient = new BlobContainerClient(
+                config["BlobCNN"], "m3globoimages");
+
+            var blobClient = containerClient.GetBlobClient(
+                model.ImageFile.FileName); // USE a temporary file name
+
+            var result = await blobClient.UploadAsync(model.ImageFile.OpenReadStream(),
+                new BlobHttpHeaders
+                {
+                    ContentType = model.ImageFile.ContentType,
+                    CacheControl = "public"
+                },
+                new Dictionary<string, string> { { "customName",
+                    model.Name} }
+            );
+
+            return RedirectToAction("Index");
         }
     }
 }
