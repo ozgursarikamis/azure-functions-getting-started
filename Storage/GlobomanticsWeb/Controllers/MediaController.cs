9c17e1b195faeccc6a4da7d48daf82400c721606
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-09-02 00:44:50 +0300

    using SAS (Shared Access Signatures) Tokens

--- Storage/GlobomanticsWeb/Controllers/MediaController.cs
+++ Storage/GlobomanticsWeb/Controllers/MediaController.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
+using Azure.Storage.Sas;
 using GlobomanticsWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -60,6 +62,43 @@ namespace GlobomanticsWeb.Controllers
             }
 
             return View(images);
+        }
+
+        [HttpGet]
+        //[Authorize] // when using auth to make sure they should get the link
+        public IActionResult Detail(string imageFileName)
+        {
+            var model = new ImageModel();
+            //validate user is authenticated before showing the image!!
+
+            //get image from storage and set URL and metadata name on model
+            var containerClient = new BlobContainerClient(
+                config["BlobCNN"], "m3globoimages");
+
+            var blob = containerClient.GetBlobClient(imageFileName);
+
+            var builder = new BlobSasBuilder
+            {
+                BlobContainerName = containerClient.Name,
+                BlobName = blob.Name,
+                ExpiresOn = DateTime.UtcNow.AddMinutes(2),
+                Protocol = SasProtocol.Https
+            };
+            builder.SetPermissions(BlobSasPermissions.Read);
+
+            var uBuilder = new UriBuilder(blob.Uri)
+            {
+                Query = builder.ToSasQueryParameters(
+                    new Azure.Storage.StorageSharedKeyCredential(
+                        containerClient.AccountName,
+                        config["BlobKey"]
+                    )).ToString()
+            };
+
+            model.Url = uBuilder.Uri.ToString();
+            model.ImageFileName = imageFileName;
+
+            return View(model);
         }
     }
 }
