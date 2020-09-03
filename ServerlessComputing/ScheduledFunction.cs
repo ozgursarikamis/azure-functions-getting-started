8380085ed922875cc97fe89a0fc20185b736be74
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-09-04 02:02:01 +0300

    Create ScheduledFunction.cs

---
+++ ServerlessComputing/ScheduledFunction.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace ServerlessComputing
+{
+    public static class ScheduledFunction
+    {
+        [FunctionName("ScheduledFunction")]
+        public static async Task Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, 
+            [Table("todos", Connection = "AzureWebJobsStorage")] CloudTable todoTable,
+            ILogger log)
+        {
+            var query = new TableQuery<TodoTableEntity>();
+            var segment = await todoTable.ExecuteQuerySegmentedAsync(query, null);
+            var deleted = 0;
+            foreach (var todo in segment)
+            {
+                if (!todo.IsCompleted)
+                {
+                    log.LogWarning("Not Deleted");
+                    continue;
+                }
+                await todoTable.ExecuteAsync(TableOperation.Delete(todo));
+                deleted++;
+            }
+            log.LogInformation($"Deleted {deleted} items at {DateTime.Now}");
+        }
+    }
+}
