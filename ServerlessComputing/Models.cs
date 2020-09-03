52ca85cde60417f3a43f02aad442fa172637165e
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-09-04 00:37:00 +0300

    Models and Mapping for Table Storage implementation

--- ServerlessComputing/Models.cs
+++ ServerlessComputing/Models.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using Microsoft.WindowsAzure.Storage.Table;
 
 namespace ServerlessComputing
 {
     public class Todo
     {
         public string Id { get; set; } = Guid.NewGuid().ToString("n");
-        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public DateTime CreatedTime { get; set; } = DateTime.UtcNow;
         public string TaskDescription { get; set; }
         public bool IsCompleted { get; set; }
     }
@@ -19,5 +20,38 @@ namespace ServerlessComputing
     {
         public bool IsCompleted { get; set; }
         public string TaskDescription { get; set; }
+    }
+
+    public class TodoTableEntity : TableEntity
+    {
+        public DateTime CreatedTime { get; set; }
+        public string TaskDescription { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+
+    public static class Mappings
+    {
+        public static TodoTableEntity ToTableEntity(this Todo todo)
+        {
+            return new TodoTableEntity
+            {
+                PartitionKey = "TODO",
+                RowKey = todo.Id,
+                CreatedTime = todo.CreatedTime,
+                IsCompleted = todo.IsCompleted,
+                TaskDescription = todo.TaskDescription
+            };
+        }
+
+        public static Todo ToTodo(this TodoTableEntity todo)
+        {
+            return new Todo
+            {
+                Id = todo.RowKey,
+                CreatedTime = todo.CreatedTime,
+                IsCompleted = todo.IsCompleted,
+                TaskDescription = todo.TaskDescription
+            };
+        }
     }
 }
